495effb0534ecdfc809d34449f6594af3fabb107
yummmmpai <spai33@gmail.com>
2016-08-09 14:26:44 -0400

    first commit

---
+++ sketch_jun20a.ino
@@ -0,0 +1,159 @@
+#include <NewPing.h>
+#include <FatReader.h>
+#include <SdReader.h>
+#include <avr/pgmspace.h>
+#include "WaveUtil.h"
+#include "WaveHC.h"
+ 
+ 
+SdReader card;    // This object holds the information for the card
+FatVolume vol;    // This holds the information for the partition on the card
+FatReader root;   // This holds the information for the filesystem on the card
+FatReader f;      // This holds the information for the file we're play
+ 
+WaveHC wave;      // This is the only wave (audio) object, since we will only play one at a time
+
+char* songs[] = {"brief.WAV", "genius.WAV", "giveitup.WAV","gold.WAV", "gross.WAV", "lion.WAV", "offended.WAV", "priceless.WAV", "promotion.WAV", "proud.WAV", "stinker.WAV", "theworst.WAV", "titanium.WAV"};
+
+//button 
+#define DEBOUNCE 100  // button debouncer
+
+//ultrasonic sensor
+#define TRIGGER_PIN 8
+#define ECHO_PIN 7
+#define MAX_DISTANCE 100
+
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); //NewPing setup of pins and max distance
+ 
+// this handy function will return the number of bytes currently free in RAM, great for debugging!   
+int freeRam(void)
+{
+  extern int  __bss_end; 
+  extern int  *__brkval; 
+  int free_memory; 
+  if((int)__brkval == 0) {
+    free_memory = ((int)&free_memory) - ((int)&__bss_end); 
+  }
+  else {
+    free_memory = ((int)&free_memory) - ((int)__brkval); 
+  }
+  return free_memory; 
+} 
+ 
+void sdErrorCheck(void)
+{
+  if (!card.errorCode()) return;
+  putstring("\n\rSD I/O error: ");
+  Serial.print(card.errorCode(), HEX);
+  putstring(", ");
+  Serial.println(card.errorData(), HEX);
+  while(1);
+}
+ 
+void setup() {
+  // set up serial port
+  Serial.begin(9600);
+  putstring_nl("WaveHC with 6 buttons");
+  
+  putstring("Free RAM: ");       // This can help with debugging, running out of RAM is bad
+  Serial.println(freeRam());      // if this is under 150 bytes it may spell trouble!
+  
+  // Set the output pins for the DAC control. This pins are defined in the library
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+ 
+  // pin13 LED
+  pinMode(13, OUTPUT);
+
+  randomSeed(analogRead(A0));
+ 
+  // enable pull-up resistors on switch pins (analog inputs)
+  //digitalWrite(14, HIGH);
+  digitalWrite(15, HIGH);
+  digitalWrite(16, HIGH);
+  digitalWrite(17, HIGH);
+  digitalWrite(18, HIGH);
+  digitalWrite(19, HIGH);
+ 
+  //  if (!card.init(true)) { //play with 4 MHz spi if 8MHz isn't working for you
+  if (!card.init()) {         //play with 8 MHz spi (default faster!)  
+    putstring_nl("Card init. failed!");  // Something went wrong, lets print out why
+    sdErrorCheck();
+    while(1);                            // then 'halt' - do nothing!
+  }
+  
+  // enable optimize read - some cards may timeout. Disable if you're having problems
+  card.partialBlockRead(true);
+ 
+// Now we will look for a FAT partition!
+  uint8_t part;
+  for (part = 0; part < 5; part++) {     // we have up to 5 slots to look in
+    if (vol.init(card, part)) 
+      break;                             // we found one, lets bail
+  }
+  if (part == 5) {                       // if we ended up not finding one  :(
+    putstring_nl("No valid FAT partition!");
+    sdErrorCheck();      // Something went wrong, lets print out why
+    while(1);                            // then 'halt' - do nothing!
+  }
+  
+  // Lets tell the user about what we found
+  putstring("Using partition ");
+  Serial.print(part, DEC);
+  putstring(", type is FAT");
+  Serial.println(vol.fatType(),DEC);     // FAT16 or FAT32?
+  
+  // Try to open the root directory
+  if (!root.openRoot(vol)) {
+    putstring_nl("Can't open root dir!"); // Something went wrong,
+    while(1);                             // then 'halt' - do nothing!
+  }
+  
+  // Whew! We got past the tough parts.
+  putstring_nl("Ready!");
+}
+ 
+void loop() {
+  int randomNumber;
+  
+    if (sonar.ping_cm() < 50 && sonar.ping_cm() > 5) {
+      randomNumber = random(1, 13);
+      playcomplete(songs[randomNumber]);
+      delay(1000);
+    }
+}
+
+
+ 
+// Plays a full file from beginning to end with no pause.
+void playcomplete(char *name) {
+  // call our helper to find and play this name
+  playfile(name);
+  Serial.println(name);
+  while (wave.isplaying) {
+  // do nothing while its playing
+  }
+  // now its done playing
+}
+ 
+void playfile(char *name) {
+
+  // see if the wave object is currently doing something
+  if (wave.isplaying) {// already playing something, so stop it!
+    wave.stop(); // stop it
+  }
+  // look in the root directory and open the file
+  if (!root.openRoot(vol)) {
+    putstring("Couldn't open file "); Serial.print(name); return;
+    while(1);
+  }
+  // OK read the file and turn it into a wave object
+  if (!wave.create(f)) {
+    putstring_nl("Not a valid WAV"); return;
+  }
+  
+  // ok time to play! start playback
+  wave.play();
+}
